eae162d63b91861e0da0777e6498e850adb4ee33
xjlonly <xjlonly@foxmail.com>
2016-12-22 13:31:32 +0800

    修改程序集版本

--- XORMMySql/MySQL.NBS/Properties/AssemblyInfo.cs
+++ XORMMySql/MySQL.NBS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("代码生成器v3.1")]
-[assembly: AssemblyDescription("代码生成器v3.1")]
+[assembly: AssemblyTitle("MySQL代码生成器v1.0")]
+[assembly: AssemblyDescription("MySQL代码生成器v1.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("代码生成器v3.1")]
-[assembly: AssemblyProduct("代码生成器v3.1")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCompany("MySQL代码生成器v1.0")]
+[assembly: AssemblyProduct("MySQL代码生成器v1.0")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
